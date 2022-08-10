0cefbf7db6d4590ade55896bfbdd413be879bf24
Daniel Yoo <dacapo0509@gmail.com>
2022-08-09 19:47:58 -0700

    bonus done

--- practice-for-week-11-sequelize-transactions-short-practice/server/app.js
+++ practice-for-week-11-sequelize-transactions-short-practice/server/app.js
@@ -69,40 +69,47 @@ app.get("/managed", async (req, res, next) => {
 	try {
 		// Create a transaction with a callback function
 		// If the function executes successfully, the transaction is committed
-
-		// Your code here
-
+		const result = await sequelize.transaction(async (t) => {
+			let rose = await Account.findOne({
+				where: {
+					firstName: "Rose",
+					lastName: "Tyler",
+				},
+			});
+			await rose.update(
+				{
+					balance: rose.balance + 200,
+				},
+				{ transaction: t }
+			);
+			await rose.save();
+			let amy = await Account.findOne({
+				where: {
+					firstName: "Amy",
+					lastName: "Pond",
+				},
+			});
+			await amy.update(
+				{
+					balance: amy.balance - 200,
+				},
+				{ transaction: t }
+			);
+			await amy.save();
+			let allAccounts = await Account.findAll({
+				order: [["firstName", "ASC"]],
+			});
+			return res.json(allAccounts);
+		});
+	} catch (error) {
 		// Queries to be performed in the transaction:
-
+		// await t.rollback();
 		// Find Rose's account, add 200 to her balance, then save
-		let rose = await Account.findOne({
-			where: {
-				firstName: "Rose",
-				lastName: "Tyler",
-			},
-		});
-		await rose.update({
-			balance: rose.balance + 200,
-		});
-		await rose.save();
 
 		// Find Amy's account, subtract 200 from her balance, then save
-		let amy = await Account.findOne({
-			where: {
-				firstName: "Amy",
-				lastName: "Pond",
-			},
-		});
-		await amy.update({
-			balance: amy.balance - 200,
-		});
-		await amy.save();
 
 		// After the transaction, formulate the response
 		// Find all accounts, ordered by firstName, returned as a JSON response
-		let allAccounts = await Account.findAll({ order: [["firstName", "ASC"]] });
-		res.json(allAccounts);
-	} catch (error) {
 		// If an error was thrown in the transaction, it will be rolled back
 		// automatically
 
